0057dfcf50bb42a16cbb3d76c805b9b127ac2615
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-25 13:03:51 -0300

    Fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,23 +11,23 @@ env.hosts = ['54.226.217.39', '34.229.176.58']
 
 
 def do_deploy(archive_path):
-    '''function'''
-    if not os.path.exists(archive_path):
-        return False
-    try:
+	'''function'''
+	if not os.path.exists(archive_path):
+		return False
+	try:
 		file_no_extension = archive_path.split('.')[0]
-    	file_extension = archive_path.split('/')[1]
-    	input_path = "/data/web_static/releases/{}/".format(file_no_extension)
+		file_extension = archive_path.split('/')[1]
+		input_path = "/data/web_static/releases/{}/".format(file_no_extension)
   
-        put(archive_path, "/tmp/")
-        run("sudo mkdir -p {}".format(input_path))
-        run("sudo tar -zxvf /tmp/{} -C {}".format(file_extension, input_path))
-        run("sudo rm -rf /tmp/{}".format(file_extension))
-        run("sudo mv -n {}/web_static/* {}".format(input_path, input_path))
-        run("sudo rm -rf {}/web_static".format(input_path))
-        run("sudo rm /data/web_static/current")
-        run("sudo ln -s {} /data/web_static/current".format(input_path))
-        return True
-
-    except Exception:
-        return False
+		put(archive_path, "/tmp/")
+		run("sudo mkdir -p {}".format(input_path))
+		run("sudo tar -zxvf /tmp/{} -C {}".format(file_extension, input_path))
+		run("sudo rm -rf /tmp/{}".format(file_extension))
+		run("sudo mv -n {}/web_static/* {}".format(input_path, input_path))
+		run("sudo rm -rf {}/web_static".format(input_path))
+		run("sudo rm /data/web_static/current")
+		run("sudo ln -s {} /data/web_static/current".format(input_path))
+		return True
+		
+	except Exception:
+		return False
