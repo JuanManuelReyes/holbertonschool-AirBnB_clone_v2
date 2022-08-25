7824e8573969f3138775bb7c6024756ee8328aa6
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-08-25 04:20:16 +0000

    Files

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#comment
+
+sudo apt update
+sudo apt -y install nginx
+
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+
+sudo touch /data/web_static/releases/test/index.html
+
+echo "
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+" > /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+
+sudo sed -i "50i location /hbnb_static/ {\nalias /data/web_static/current/;\n}\n" /etc/nginx/sites-enabled/default
+
+sudo service nginx restart
