1af1a2003014a955f78439adc6f298ce0726c04d
JuanManuelReyes <juanmanuelreyesarrambide@gmail.com>
2022-08-25 10:00:42 -0700

    Pycode

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -9,13 +9,12 @@ import datetime
 
 
 def do_pack():
-	"""comment"""
-	try:
-		time = datetime.datetime.now()
-		date = time.strftime("%Y%m%d%H%M%S")
-		local("mkdir -p versions")
-		local("tar -cvzf versions/web_static_{}.tgz web_static/"
-			.format(date))
-		return("versions/web_static_{}.tgz".format(date))
-	except:
-		return None
+    """comment"""
+    try:
+        time = datetime.datetime.now()
+        date = time.strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(date))
+        return("versions/web_static_{}.tgz".format(date))
+    except exception:
+        return None
