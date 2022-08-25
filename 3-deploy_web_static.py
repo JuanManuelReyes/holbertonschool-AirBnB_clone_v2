b990caa972215d10ac69dc7994ca7ef2dc8ee4f5
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-08-25 14:02:14 -0300

    Task 3

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,40 +12,39 @@ env.user = "ubuntu"
 env.hosts = ['54.226.217.39', '34.229.176.58']
 
 def do_pack():
-	"""comment"""
-	try:
-		time = datetime.datetime.now()
-		date = time.strftime("%Y%m%d%H%M%S")
-		local("mkdir -p versions")
-		local("tar -cvzf versions/web_static_{}.tgz web_static/"
-			.format(date))
-		return("versions/web_static_{}.tgz".format(date))
-	except:
-		return None
+    """comment"""
+    try:
+        time = datetime.datetime.now()
+        date = time.strftime("%Y%m%d%H%M%S")
+        local("mkdir -p versions")
+        local("tar -cvzf versions/web_static_{}.tgz web_static/".format(date))
+        return("versions/web_static_{}.tgz".format(date))
+    except exception:
+        return None
+
 
 def do_deploy(archive_path):
-	"""Comment"""
-	if exists(archive_path) is False:
-		return False
-	try:
-		put(archive_path, "/tmp/")
-  
-		filename = archive_path.split("/")[1]
-		filename_no_extension = filename.split(".")[0]
-		file_path = "/data/web_static/releases/" + filename_no_extension + "/"
-  
-		run("mkdir -p " + file_path)
-		run("tar -xzf /tmp/" + filename + " -C " + file_path)
-		run("mv " + file_path + "web_static/*" + " " + file_path)
-		run("rm /tmp/{}".format(filename))
-		run("rm -rf " + file_path + "web_static")
-		run("rm -rf /data/web_static/current")
-		run("ln -s " + file_path + " /data/web_static/current")
-		print("New version deployed!")
-		return True
-		
-	except Exception:
-		return False
+    """Comment"""
+    if exists(archive_path) is False:
+        return False
+    try:
+        put(archive_path, "/tmp/")
+        filename = archive_path.split("/")[1]
+        filename_no_extension = filename.split(".")[0]
+        file_path = "/data/web_static/releases/" + filename_no_extension + "/"
+        run("mkdir -p " + file_path)
+        run("tar -xzf /tmp/" + filename + " -C " + file_path)
+        run("mv " + file_path + "web_static/*" + " " + file_path)
+        run("rm /tmp/{}".format(filename))
+        run("rm -rf " + file_path + "web_static")
+        run("rm -rf /data/web_static/current")
+        run("ln -s " + file_path + " /data/web_static/current")
+        print("New version deployed!")
+        return True
+
+    except Exception:
+        return False
+
 
 def deploy():
     """comment"""
