cf18f9039635466f09be04686c228f229f25797d
JuanManuelReyes <manu_reyes2003@hotmail.com>
2022-09-02 15:52:58 -0300

    Task10 Fix HTML

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -6,6 +6,7 @@ Write a script that starts a Flask web application.
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+from models.city import City
 
 app = Flask(__name__)
 
